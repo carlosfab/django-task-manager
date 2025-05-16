ce4d3e0449d050177efb973c6303996f2708ff6a
Carlos Melo <carlos.fab@gmail.com>
2025-05-16 19:28:07 -0400

    Changed some files

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
 
 # Create your tests here.
+for i in range(10):
+	print(i)
+class ExampleTestCase(TestCase):
+	def test_example(self):
+		self.assertEqual(1 + 1, 2)
+
+	def test_another_example(self):
+		self.assertTrue(True)
